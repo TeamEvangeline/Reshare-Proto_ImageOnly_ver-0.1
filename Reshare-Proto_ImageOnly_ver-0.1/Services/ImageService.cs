291d653b7478dbbffdaed6a134af4de36d765dac
Sudhansu812 <sudhansumaharana812@gmail.com>
2022-02-24 09:27:14 +0530

    services

---
+++ Reshare-Proto_ImageOnly_ver-0.1/Services/ImageService.cs
@@ -0,0 +1,8 @@
+﻿using Reshare_Proto_ImageOnly_ver_0._1.Services.Interfaces;
+
+namespace Reshare_Proto_ImageOnly_ver_0._1.Services
+{
+    public class ImageService : IImageService
+    {
+    }
+}
---
+++ Reshare-Proto_ImageOnly_ver-0.1/Services/Interfaces/IImageService.cs
@@ -0,0 +1,6 @@
+﻿namespace Reshare_Proto_ImageOnly_ver_0._1.Services.Interfaces
+{
+    public interface IImageService
+    {
+    }
+}
